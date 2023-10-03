8ab3aa4aabe818a8ac5ce79f5336136cc7916eb6
Candela101 <cabero01@floridauniversitaria.es>
2023-10-03 13:36:45 +0200

    subo carpeta microAnuncios

---
+++ microAnuncios/src/main/java/com/example/microAnuncios/MicroAnunciosApplication.java
@@ -0,0 +1,13 @@
+package com.example.microAnuncios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAnunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAnunciosApplication.class, args);
+	}
+
+}
---
+++ microAnuncios/src/test/java/com/example/microAnuncios/MicroAnunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microAnuncios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAnunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
